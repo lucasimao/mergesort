9ec08d078b5db8b54f439e774b4b5a08c1f09b21
yyq <yyq@me.com>
2013-07-03 10:03:32 +0800

    Initial Commit

---
+++ mergesort/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  mergesort
+//
+//  Created by Yang YanQing on 13-7-3.
+//  Copyright (c) 2013年 Yang YanQing. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
